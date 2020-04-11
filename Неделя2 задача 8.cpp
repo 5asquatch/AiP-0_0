2ec50fe979591d741343817464886aeb2a9e7978
5asquatch <61937286+5asquatch@users.noreply.github.com>
2020-04-11 22:14:29 +0300

    Add files via upload

---
+++ Неделя2 задача 8.cpp
@@ -0,0 +1,24 @@
+﻿
+#include "pch.h"
+#include <iostream>
+using namespace std;
+
+
+int x = -2;
+int xv5;
+int sum;
+
+
+
+int main(){
+	xv5 = x * x * x * x * x ;
+	sum = abs(x) + xv5;
+
+	cout << sum << endl;
+
+	
+	
+
+}
+
+
