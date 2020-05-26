7488ebf37a45b6f115e1e6c62a11ea7f81a9686b
5asquatch <61937286+5asquatch@users.noreply.github.com>
2020-05-26 16:52:48 +0300

    Add files via upload

---
+++ Неделя 6 задача 9.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>     
+#include <stdlib.h>
+#include <time.h>
+
+
+int main()
+{
+	
+	int arr[15], c = 0;
+
+	srand(time(0));
+	for (int i = 0; i < 15; i++)
+	{
+		arr[i] = 0 + rand() % 15;
+		std::cout << arr[i] << " ";
+		if (arr[i] > 0) c++;
+	}
+	std::cout << "Count of numbers > 0 : " << c << "\n";
+	return 0;
+
+
+}
