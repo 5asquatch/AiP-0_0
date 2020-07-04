44a65874a6efc7c3cd45d61bb6cac2e18a83d4ef
5asquatch <61937286+5asquatch@users.noreply.github.com>
2020-07-04 21:07:13 +0300

    Add files via upload

---
+++ Неделя 13 задача 1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+void main()
+{
+	fstream f;
+	f.open("Z1.txt", ios::out | ios::trunc);
+	string s1 = "Suma sosumhol suma um";
+	f << s1 << endl;
+	for (int i = 0; i < s1.length(); i++)
+	{
+		if (s1[i] == 'u' && s1[i + 1] == 'm' && s1[i - 1] != ' ')
+		{
+			s1.erase(i, 2);
+		}
+	}
+	cout << s1;
+	f << s1;
+}
